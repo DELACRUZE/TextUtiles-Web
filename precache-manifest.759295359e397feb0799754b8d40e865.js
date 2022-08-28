ca154d868564d9e2b0958f2536ad8669908b2d07
Aman <mirzaaman508@gamil.com>
2022-08-28 02:29:15 -0700

    Updates

---
+++ precache-manifest.759295359e397feb0799754b8d40e865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4dd9aaf99339ff9b5df",
+    "url": "/TextUtiles-Web/static/css/main.add2557e.chunk.css"
+  },
+  {
+    "revision": "a4dd9aaf99339ff9b5df",
+    "url": "/TextUtiles-Web/static/js/main.a4dd9aaf.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/TextUtiles-Web/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "279a9e52405bebe0ee46",
+    "url": "/TextUtiles-Web/static/js/2.279a9e52.chunk.js"
+  },
+  {
+    "revision": "2734cf083985985a7ace",
+    "url": "/TextUtiles-Web/static/js/runtime~main.2734cf08.js"
+  },
+  {
+    "revision": "129c932f671443e4d868264c67767672",
+    "url": "/TextUtiles-Web/index.html"
+  }
+];
